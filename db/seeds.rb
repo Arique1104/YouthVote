342862f8b6dd1be56ba78a5d1364b69b26f96ab6
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2024-02-01 06:32:26 -0800

    Establish initial seeds to determine the relationship between data tables.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# registered student user
+ student1 = User.create!(name: 'Student User', email: 'student-user@school.com', role: "student")
+ # registered student w/ campus relationship
+ student1.campus.create!(user.id, campus.id, dorm.id)
+ # student dorm captain
+ student 2 = User.create!(name: 'Student Dorm Captain', email: 'student-dorm-captain@school.com', role: "student leader")
+ location = student2.location.create(campus.id, dorm.id)
+ # organizer
+ User.create!(name: 'Organizer', email: 'organizer@org.com', role: "organizer")
+ 
+ # leadership
+ User.create!(name: 'Leadership', email: 'leadership@org.com', role: 'leadership')
