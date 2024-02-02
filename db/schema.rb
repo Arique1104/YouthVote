e09be320c68d1538849bcb30a9450485710cc823
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2024-02-02 10:48:46 -0800

    Adds Institution Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_01_181859) do
-  create_table "users", force: :cascade do |t|
+ActiveRecord::Schema[7.1].define(version: 2024_02_02_183126) do
+  create_table "institutions", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'pry'
 
-# registered student user
- student1 = User.create!(name: 'Student User', email: 'student-user@school.com', role: "student")
- # registered student w/ campus relationship
- student1.campus.create!(user.id, campus.id, dorm.id)
- # student dorm captain
- student 2 = User.create!(name: 'Student Dorm Captain', email: 'student-dorm-captain@school.com', role: "student leader")
- location = student2.location.create(campus.id, dorm.id)
- # organizer
- User.create!(name: 'Organizer', email: 'organizer@org.com', role: "organizer")
- 
- # leadership
- User.create!(name: 'Leadership', email: 'leadership@org.com', role: 'leadership')
+institution1 = Institution.create!(name: 'University of Pennsylvania')
+binding.pry;
+institution1.county.create!(name: 'Philadelphia')
+institution2 = Institution.create!(name: 'Temple University', county: "Philadelphia County")
+
+county2 = County.create!(name: 'Adams')
